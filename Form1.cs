9193072a916b58ae63bf2f2ea1d2b059ae6e6000
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-04 07:30:16 +0700

    Menambahkan Layout GUI Calculator

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace MOD3_103022300112
 {
     public partial class Form1 : Form
@@ -5,6 +7,68 @@ namespace MOD3_103022300112
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int b = 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int c = 3;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int d = 4;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int j = 5;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int f = 6;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int g = 7;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int h = 8;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            int i = 9;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            int j = 0;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+            
+            
+        }
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+            
         }
     }
 }
